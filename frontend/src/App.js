01b0a4e476e2a2d92ca268aae7117c0dfb78cf7f
Yusuke Shimizu <159997300+isdlyusukeshimizu@users.noreply.github.com>
2024-06-08 12:15:41 +0900

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// import Home from './Home';
+import Signup from './components/signup';
+import Login from './components/login';
+ 
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/signup" component={Signup} />
+        <Route path="/login" component={Login} />
+      </Switch>
+    </Router>
+  );
+}
+ 
+export default App;
